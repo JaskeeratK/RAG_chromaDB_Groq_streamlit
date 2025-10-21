96cdfc3cfe4d527ffee9f97144e370f814580b7e
Jaskeerat <thejaskeerat@gmail.com>
2025-10-21 21:16:02 +0530

    Update rag_core.py

--- rag_core.py
+++ rag_core.py
@@ -2,7 +2,8 @@ import os
 from langchain_community.document_loaders import PyPDFLoader, CSVLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from langchain_core.runnables import Runnable
 from langchain_groq import ChatGroq
